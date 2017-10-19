bf2ecc291a962e52da86387cb097c5f66bda794e
Ross Glover <ross@ross.mayfirst.org>
2017-10-19 13:20:52 -0400

    Model for PokerSession and associated migration.

--- chronicle/tests.py
+++ chronicle/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from chronicle.views import home_page
+from chronicle.models import PokerSession
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
@@ -13,4 +14,22 @@ class HomePageTest(TestCase):
         self.assertIn('A new sess chronicle', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
                         
-
+class PokerSessionModelTest(TestCase):
+    def test_saving_and_retrieving_pokersessions(self):
+        first_pokersession = PokerSession()
+        first_pokersession.text = 'The first (ever) poker session'
+        first_pokersession.save()
+        second_pokersession = PokerSession()
+        second_pokersession.text = 'Session the second'
+        second_pokersession.save()
+        saved_pokersessions = PokerSession.objects.all()
+        self.assertEqual(saved_pokersessions.count(), 2)
+        first_saved_pokersession = saved_pokersessions[0]
+        second_saved_pokersession = saved_pokersessions[1]
+        self.assertEqual(first_saved_pokersession.text,
+                         'The first (ever) poker session'
+        )
+        self.assertEqual(second_saved_pokersession.text,
+                         'Session the second'
+        )
+        
