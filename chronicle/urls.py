be0707d434041bdbcfbe96bce814407d3608ce70
Ross Glover <ross@ross.mayfirst.org>
2017-10-22 12:24:54 -0400

    Refactored urls to use url.py in chronicle app.

--- chronicle/urls.py
+++ chronicle/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
-from chronicle import views as chronicle_views
-from chronicle import urls as chronicle_urls
+from chronicle import views
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
-    url(r'^$', chronicle_views.home_page, name='home'),
-    url(r'^chronicles/', include(chronicle_urls)),
+    url(r'^new$', views.new_chronicle, name='new_chronicle'),
+    url(r'^(\d+)/$', views.view_chronicles, name='view_chronicles'),
+    url(r'^(\d+)/add_sess', views.add_sess, name='add_sess'),
 ]
